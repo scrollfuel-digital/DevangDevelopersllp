f786bdaea401b358912c8d7f2c2276609ddedf27
Krushna Sondhiya <sondhiyakrushna@gmail.com>
2026-07-24 10:40:01 +0530

    correction in projects

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import react from 'react'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { createRoot } from 'react-dom/client'
-createRoot(document.getElementById('root')).render(
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
-)
+);
